0db5e44691d42922ba686a48009cd2764393c35e
Korakrit Chariyasathian <13772625+korakrit-c@users.noreply.github.com>
2021-10-05 15:39:50 +0700

    Init project

---
+++ main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"bank-transfer/handler"
+	"bank-transfer/repository"
+	"bank-transfer/service"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+	"github.com/jmoiron/sqlx"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	initConfig()
+	initTimeZone()
+	db := initDatabase()
+
+	customerRepository := repository.NewCustomerRepositoryDB(db)
+	customerRepositoryMock := repository.NewCustomerRepositoryMock()
+	_ = customerRepository
+	_ = customerRepositoryMock
+
+	customerService := service.NewCustomerService(customerRepositoryMock)
+	customerHandler := handler.NewCustomerHandler(customerService)
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/customers", customerHandler.GetCustomers).Methods(http.MethodGet)
+	router.HandleFunc("/customers/{customerID:[0-9]+}", customerHandler.GetCustomer).Methods(http.MethodGet)
+
+	log.Printf("Banking service started at port %v", viper.GetInt("app.prot"))
+	http.ListenAndServe(fmt.Sprintf(":%v", viper.GetInt("app.prot")), router)
+}
+
+func init() {
+
+}
+
+func initConfig() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func initTimeZone() {
+	ict, err := time.LoadLocation("Asia/Bangkok")
+	if err != nil {
+		panic(err)
+	}
+
+	time.Local = ict
+}
+
+func initDatabase() *sqlx.DB {
+	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v",
+		viper.GetString("db.username"),
+		viper.GetString("db.password"),
+		viper.GetString("db.host"),
+		viper.GetInt("db.port"),
+		viper.GetString("db.database"),
+	)
+
+	db, err := sqlx.Open(viper.GetString("db.driver"), dsn)
+	if err != nil {
+		panic(err)
+	}
+
+	db.SetConnMaxLifetime(3 * time.Minute)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+
+	return db
+}
---
+++ service/custoemr_service.go
@@ -0,0 +1,50 @@
+package service
+
+import (
+	"bank-transfer/repository"
+	"log"
+)
+
+type customerService struct {
+	custRepo repository.CustomerRepository
+}
+
+func NewCustomerService(custRepo repository.CustomerRepository) CustomerService {
+	return customerService{custRepo: custRepo}
+}
+
+func (s customerService) GetCustomers() ([]CustomerResponse, error) {
+	customers, err := s.custRepo.GetAll()
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	custResponses := []CustomerResponse{}
+	for _, customer := range customers {
+		custResponse := CustomerResponse{
+			CustomerID: customer.CustomerID,
+			Name:       customer.Name,
+			Status:     customer.Status,
+		}
+		custResponses = append(custResponses, custResponse)
+	}
+
+	return custResponses, nil
+}
+
+func (s customerService) GetCustomer(id int) (*CustomerResponse, error) {
+	customer, err := s.custRepo.GetById(id)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	custResponse := CustomerResponse{
+		CustomerID: customer.CustomerID,
+		Name:       customer.Name,
+		Status:     customer.Status,
+	}
+
+	return &custResponse, nil
+}
